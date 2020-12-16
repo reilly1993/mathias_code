0aa2995843c8c36e702ebb69b3d02511b042c9b9
Christian Vingborg <vingborg10@gmail.com>
2020-12-16 10:06:18 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import PropTypes from "prop-types";
+
+const TheContext = createContext();
 
 function App() {
+  const [state2, setState2] = useState("bla");
+  const handleAnotherClick = useCallback(() => {
+    setState2("bluh");
+  }, []);
+
+  const [state, setState] = useState("initial");
+  const handleClick = useCallback(() => {
+    if (state === "initial") {
+      setState("clicked");
+    } else {
+      setState("initial");
+    }
+  }, [state]);
+
+  useEffect(() => {
+    console.log("ran", state2);
+  }, [state, state2]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <TheContext.Provider value={state}>
+      <div>
+        <button onClick={handleAnotherClick}>Yo</button>
+        <button onClick={handleClick}>Hey</button>
+        {state === "clicked" && <MessageButton onClick={handleClick} />}
+      </div>
+    </TheContext.Provider>
+  );
+}
+
+function MessageButton({ onClick }) {
+  return (
+    <div>
+      <button onClick={onClick}>hey</button>
+      <SomeThang />
     </div>
   );
 }
+
+const SomeThang = () => {
+  const context = useContext(TheContext);
+  return <div>some thang {context}</div>;
+};
+
+MessageButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default App;
